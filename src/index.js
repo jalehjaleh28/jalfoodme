8372830afa375bd9cff805ae246a83a86dcf2354
jalehjaleh28 <94356744+jalehjaleh28@users.noreply.github.com>
2022-04-21 12:18:10 +0200

    change index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import './App.css';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import {BrowserRouter} from "react-router-dom";
+import Routess from './componnets/Routs';
 import Nav from './componnets/nav';
 
-import Baner from './componnets/baner';
 
-function App() {
-  return (
-    <div className="App">
-     <Nav/>
-     <Baner/>
-     
-    </div>
-  );
-}
-
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Nav/>
+      <Routess/>
+    </BrowserRouter>
+  
+    </React.StrictMode>
+);
